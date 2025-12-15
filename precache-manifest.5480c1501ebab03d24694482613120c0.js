423fe718cdab78d8471feacc76fe932d29b5fe71
Jessica Leach <jessicamaryleach@gmail.com>
2025-12-15 13:43:36 +0000

    Updates

---
+++ precache-manifest.5480c1501ebab03d24694482613120c0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6dc073acb1d409daf5cc0f1d2df55b4e",
+    "url": "/drive-for-an-hour/index.html"
+  },
+  {
+    "revision": "281f02bdacc666bd39a6",
+    "url": "/drive-for-an-hour/static/css/2.cee453c7.chunk.css"
+  },
+  {
+    "revision": "25bf04435fdd30d3cf8c",
+    "url": "/drive-for-an-hour/static/css/main.4f25784b.chunk.css"
+  },
+  {
+    "revision": "281f02bdacc666bd39a6",
+    "url": "/drive-for-an-hour/static/js/2.f236ff7e.chunk.js"
+  },
+  {
+    "revision": "53433c21199e3f302ab2e9e70b620b5f",
+    "url": "/drive-for-an-hour/static/js/2.f236ff7e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25bf04435fdd30d3cf8c",
+    "url": "/drive-for-an-hour/static/js/main.3a2431f2.chunk.js"
+  },
+  {
+    "revision": "edb0ae596097cd4ca465",
+    "url": "/drive-for-an-hour/static/js/runtime-main.ffa1a3cf.js"
+  }
+]);
